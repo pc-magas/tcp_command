e89c3b65bcb27a9f272ea56dd0efcfad64745c9a
pc-magas <katanophoros@gmail.com>
2019-06-03 00:09:03 +0300

    Keep it simpler

---
+++ src/socket/simple_command.cpp
@@ -0,0 +1,14 @@
+#include "telnet.h"
+#include<string>
+
+std::string SimpleCommandHandler::readLine(int socketid){
+    
+}
+
+void SimpleCommandHandler::sendResult(int socketid, std::string result){
+
+}
+
+void SimpleCommandHandler::handle(int socketid){
+
+}
--- src/socket/simple_command.h
+++ src/socket/simple_command.h
@@ -4,15 +4,16 @@
 #include"network.h"
 #include<string>
 
-class TelnetHandler:public ConnectionHandler{
+class SimpleCommandHandler:public ConnectionHandler{
     public:
-    TelnetHandler(){};
-    ~TelnetHandler(){};
+    SimpleCommandHandler(int readBufferSize):buffLen(readBufferSize){};
+    ~SimpleCommandHandler(){};
     void handle(int socketid);
 
     private:
     std::string readLine(int socketid);
     void sendResult(int socketid, std::string result);
+    const int buffLen;
 }
 
 #endif
