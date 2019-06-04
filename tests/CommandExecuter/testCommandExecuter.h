623e3ef99339f732b6f4c903fa9b8a7a82c4e3fc
pc-magas <katanophoros@gmail.com>
2019-06-04 21:44:24 +0300

    Test failing

--- tests/CommandExecuter/testCommandExecuter.h
+++ tests/CommandExecuter/testCommandExecuter.h
@@ -16,4 +16,27 @@ class TestCommandExecuter: public CxxTest::TestSuite{
             TS_ASSERT_EQUALS(command,"");
         }
 
+        void testGetCommand(){
+            CommandParser p;
+            const char data[5] ={'e','x','i','t','\n'};
+            int length=5;
+            
+            p.addData(5,data,length);
+            std::string command=p.getCommand(5);
+            TS_ASSERT_EQUALS(command,"exit");
+        }
+
+        void testGetCommandMultiple(){
+            CommandParser p;
+            const char data[10] ={'e','x','i','t','\n','e','x','i','t','\n'};
+            int length=10;
+            
+            p.addData(5,data,length);
+            std::string command=p.getCommand(5);
+            TS_ASSERT_EQUALS(command,"exit");
+            
+            command=p.getCommand(5);
+            TS_ASSERT_EQUALS(command,"exit");
+        }
+
 };
