14161de120b73e8006be8cdbbd97f880615f4180
pc-magas <katanophoros@gmail.com>
2019-06-15 19:06:33 +0300

    Commands sucessfully parsed

--- src/tools/command_parser.cpp
+++ src/tools/command_parser.cpp
@@ -1,5 +1,6 @@
 #include "command_parser.h"
 #include<string>
+#include<iostream>
 
 void CommandParser::addData(int socketid, const char* data, int length){
     this->commandBuff[socketid].append(data,length);
@@ -13,6 +14,8 @@ std::string CommandParser::getCommand(int socketid){
     }
 
     std::string fetchedCommand = this->commandBuff[socketid].substr(0,pos);
-    this->commandBuff[socketid] = this->commandBuff[socketid].substr(pos);
+    // std::cout << "Before: " << this->commandBuff[socketid] << std::endl;
+    this->commandBuff[socketid] = this->commandBuff[socketid].substr(pos+1);
+    // std::cout << "After: " << this->commandBuff[socketid] << std::endl;
     return fetchedCommand;
 }
