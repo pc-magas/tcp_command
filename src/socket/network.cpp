a211cd4a3aad396e9dfa99ccb9c5616c9998798c
pc-magas <katanophoros@gmail.com>
2019-06-16 22:02:23 +0300

    Receive full Intial Message

--- src/socket/network.cpp
+++ src/socket/network.cpp
@@ -49,7 +49,7 @@ void TCPServer::listen(){
        }
        std::cout << "Client Socket: " << clntSock << std::endl;
        std::cout << "Handling client: " << inet_ntoa(ClntAddr.sin_addr) << std::endl;
-       send(clntSock, "WELCOME", 6, 0);
+       send(clntSock, "WELCOME", 8, 0);
 
        std::thread handleConnectionThread(callHandler, this->c, clntSock);
        handleConnectionThread.detach();
