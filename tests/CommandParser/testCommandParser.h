b53afb7f1b1dae04d0dc84c5e3627e051cb492ba
pc-magas <katanophoros@gmail.com>
2019-06-16 12:03:20 +0300

    Test Parser

--- tests/CommandParser/testCommandParser.h
+++ tests/CommandParser/testCommandParser.h
@@ -16,12 +16,41 @@ class TestCommandParser: public CxxTest::TestSuite{
             TS_ASSERT_EQUALS(command,"");
         }
 
+        void testGetCommandEmpty(){
+            CommandParser p;
+            const char data[5] ={};
+            int length=5;
+            
+            p.addData(5,data,length);
+            std::string command=p.getCommand(5);
+            TS_ASSERT_EQUALS(command,"");
+        }
+
         void testGetCommand(){
             CommandParser p;
             const char data[5] ={'e','x','i','t','\n'};
             int length=5;
             
             p.addData(5,data,length);
+            std::string command=p.getCommand(5);
+            TS_ASSERT_EQUALS(command,"exit");
+        }
+
+        void getTestCOmmandCHunked(){
+            CommandParser p;
+            const char chunk_one[2]={'e','x'};
+            int length_one=2;
+
+            const char chunk_two[1]={'i'};
+            int length_two=1;
+
+            const char chunk_three[2]={'e','x'};
+            int length_three=2;
+
+            p.addData(5,chunk_one,length_one);
+            p.addData(5,chunk_two,length_two);
+            p.addData(5,chunk_three,length_three);
+            
             std::string command=p.getCommand(5);
             TS_ASSERT_EQUALS(command,"exit");
         }
